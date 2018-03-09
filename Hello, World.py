e5cd195422f69b6a21f09f06b43a2613b4c89c64
fanad <37211442+fanad@users.noreply.github.com>
2018-03-09 16:47:07 +0600

    Добавили новый код

---
+++ Hello, World.py
@@ -0,0 +1,16 @@
+number = int(input("Введите число 1-12"))
+if number == 1:
+    print("Январь")
+elif number == 2;
+    print("феварль")
+elif number == 3;
+    print("Март")
+elif number == 4;
+    print("Апрель")
+elif number == 5;
+    print("Май")
+elif number == 6;
+    print("Июнь")
+elif number == 7;
+    print("Июль")
+    
